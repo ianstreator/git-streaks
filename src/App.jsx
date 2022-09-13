bfdef26d611abbae5a1456efcd48dda638a86f57
Ian <istreator@yahoo.com>
2022-09-12 19:34:23 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-xl">hello</h1>
+      <button className="btn">Submit</button>
+    </div>
+  );
+}
+
+export default App;
