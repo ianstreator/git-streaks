cb0fae196db46467c50d36defbdc9be4af754b2e
Ian <istreator@yahoo.com>
2022-09-12 20:34:43 -0500

    created navbar and footer component, started working on pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-xl">hello</h1>
-      <button className="btn">Submit</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">Content</main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
