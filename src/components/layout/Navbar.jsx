85a0a95c504622a1071a5c3959421a8d74f26a4c
Ian <istreator@yahoo.com>
2022-09-15 14:18:36 -0500

    added new dependency 'daisyui'..

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,3 @@
-import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Logo from "./assets/git-streaks.svg"
--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -2,35 +2,37 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import flame from "../layout/assets/streak-fire.svg";
 
 function UserItem({ user: { login, avatar_url } }) {
   const { getUser, getUserRepos } = useContext(GithubContext);
 
   return (
-    <div className="card shadow-md compact side bg-base-100">
-      <div className="flex-row items-center space-x-4 card-body">
-        <div>
-          <div className="avatar">
-            <div className="rounded-full shadow w-14 h-14">
-              <img src={avatar_url} alt="Profile" />
+    <Link
+      className="text-base-content text-opacity-40"
+      to={`/user/${login}`}
+      onClick={() => {
+        getUser(login);
+        getUserRepos(login);
+      }}
+    >
+      <div className="card shadow-md compact side bg-base-100 glass opacity-50 hover:opacity-100">
+        <div className="flex-row items-center space-x-4 card-body">
+          <div>
+            <div className="avatar">
+              <div className="rounded-full shadow w-14 h-14">
+                <img src={avatar_url} alt="Profile" />
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <h2 className="card-title">{login}</h2>
-          <Link
-            className="text-base-content text-opacity-40"
-            to={`/user/${login}`}
-            onClick={() => {
-              getUser(login);
-              getUserRepos(login);
-            }}
-          >
+          <div>
+            <h2 className="card-title">{login}</h2>
             View Profile
-          </Link>
+          </div>
+          <img src={flame} alt="flame-icon" width={25} />
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
