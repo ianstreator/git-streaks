54bd0e3fdcc9fa33afd2dcdef50b583c318d8ae8
Ian <istreator@yahoo.com>
2022-09-21 06:29:36 -0500

    changed api URL

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -27,7 +27,7 @@ export const GithubProvider = ({ children }) => {
       console.log(items);
 
       const api_res = await fetch(
-        "https://git-streaks-api.vercel.app/api/index",
+        "https://git-streaks-api-ianstreator.vercel.app/api/index",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
