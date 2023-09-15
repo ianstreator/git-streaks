b5d5f4874b9feafb5dd6183453803c975bba6647
Ian <ianstreator+github@gmail.com>
2023-09-15 00:37:48 -0500

    updated about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,18 +6,8 @@ function About() {
       <h1 className="text-6xl mb-4">Git Streaks</h1>
       <p className="mb-4 text-2xl font-light">
         {
-          "A React app to view GitHub account contribution data ( current-streak, best-streak, yearly contributions ), and further profile details."
+          "An application built to view your and your friends' GitHub contributions."
         }
-      </p>
-      <p className="text-lg text-gray-400">
-        Layout By:
-        <a
-          className="text-white"
-          target="_blank"
-          href="https://twitter.com/hassibmoddasser"
-        >
-          &nbsp;Hassib Moddasser
-        </a>
       </p>
     </div>
   );
