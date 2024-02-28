171185e298dea5c5d423efa57e925f7941fe849d
Ian <ianstreator+github@gmail.com>
2024-02-28 12:48:41 -0600

    renaming, organization, UI tweaks

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import sleep from "../layout/assets/sleepy-coder-icon.svg";
-import yearly from "../layout/assets/yearly-contribution-icon.svg";
-import best from "../layout/assets/best-streak-icon.svg";
-import current from "../layout/assets/current-streak-icon.svg";
+import streakIcon from "../layout/assets/current-streak-icon.svg";
+import sleepyIcon from "../layout/assets/sleepy-coder-icon.svg";
+import bestIcon from "../layout/assets/best-streak-icon.svg";
+import calendarIcon from "../layout/assets/yearly-contribution-icon.svg";
 import watching from "../layout/assets/watching-coder.svg";
 import not_watching from "../layout/assets/not-watching-coder.svg";
 
@@ -13,15 +13,37 @@ import { useEffect } from "react";
 import Loader from "../layout/Loader";
 
 function UserItem({ user }) {
-  const { removeFromWatchList, addToWatchList, watchlist, users } =
-    useContext(GithubContext);
-
   const {
     login,
     avatar_url,
     userContributionData: { currentStreak, bestStreak, yearlyContributions },
     updating,
   } = user;
+
+  const cardFeatureValues = [
+    {
+      src: currentStreak > 0 ? streakIcon : sleepyIcon,
+      alt: "current-icon",
+      title: "current streak",
+      value: currentStreak,
+    },
+    {
+      src: bestIcon,
+      alt: "best-icon",
+      title: "best streak",
+      value: bestStreak,
+    },
+    {
+      src: calendarIcon,
+      alt: "calendar-icon",
+      title: "year to date contributions",
+      value: yearlyContributions,
+    },
+  ];
+
+  const { removeFromWatchList, addToWatchList, watchlist, users } =
+    useContext(GithubContext);
+
   const [watchChange, setWatchChange] = useState(watchlist[login]);
 
   useEffect(() => {
@@ -29,16 +51,16 @@ function UserItem({ user }) {
   }, [watchlist, users]);
 
   return (
-    <div className="relative card rounded-md shadow-md compact side bg-zinc-700 opacity-40 hover:opacity-100 w-96 mx-auto">
+    <div className="relative card rounded-l-full shadow-md compact side bg-zinc-700 opacity-40 hover:opacity-100 w-96 mx-auto">
       {updating && (
         <div className="absolute z-[100] w-full h-full text-xl bg-black/[0.6] text-white flex flex-row justify-center items-center">
           Updating
           <Loader />
         </div>
       )}
-      <div className="relative flex-row space-x-1 card-body">
+      <div className="relative flex-row space-x-1 card-body !p-2">
         <div className="avatar">
-          <div className="rounded-full shadow w-20 h-20">
+          <div className="rounded-full shadow w-20 h-20 m-auto">
             <img src={avatar_url} alt="Profile" />
           </div>
         </div>
@@ -65,42 +87,22 @@ function UserItem({ user }) {
               }
               className={`mx-1 ml-auto cursor-pointer`}
               onClick={() =>
-                watchlist[login] ? removeFromWatchList(user) : addToWatchList(user)
+                watchlist[login]
+                  ? removeFromWatchList(user)
+                  : addToWatchList(user)
               }
             />
           </div>
           <div className="flex justify-between bg-zinc-800 rounded bg-opacity-60 w-full">
-            <div
-              className="bg-zinc-800 pr-2 p-1 m-1 flex rounded"
-              title="current streak"
-            >
-              <img
-                src={currentStreak > 0 ? current : sleep}
-                alt="current-icon"
-                width={20}
-                className="mx-1"
-              />
-              <p className="text-2xl ml-1">{currentStreak}</p>
-            </div>
-            <div
-              className="bg-zinc-800 pr-2 p-1 m-1 flex rounded"
-              title="best streak"
-            >
-              <img src={best} alt="current-icon" width={20} className="mx-1" />
-              <p className="text-2xl ml-1">{bestStreak}</p>
-            </div>
-            <div
-              className="bg-zinc-800 pr-2 p-1 m-1 flex rounded"
-              title="year to date contributions"
-            >
-              <img
-                src={yearly}
-                alt="current-icon"
-                width={20}
-                className="mx-1"
-              />
-              <p className="text-2xl ml-1">{yearlyContributions}</p>
-            </div>
+            {cardFeatureValues.map(({ src, alt, title, value }) => (
+              <div
+                className="bg-zinc-800 pr-2 p-1 m-1 flex rounded"
+                title={title}
+              >
+                <img src={src} alt={alt} width={20} className="mx-1" />
+                <p className="text-2xl ml-1">{value}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
